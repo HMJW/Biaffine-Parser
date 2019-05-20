fe569084758d7ed21a07aaa8c0bd9ba617e665f5
zysite <zysite@outlook.com>
2019-05-20 16:37:12 +0800

    Remove the regex module

--- parser/utils/vocab.py
+++ parser/utils/vocab.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import unicodedata
 from collections import Counter
 
-import regex
 import torch
 
 
@@ -24,7 +24,7 @@ class Vocab(object):
 
         # ids of punctuation that appear in words
         self.puncts = sorted(i for word, i in self.word_dict.items()
-                             if regex.match(r'\p{P}+$', word))
+                             if self.is_punctuation(word))
 
         self.n_words = len(self.words)
         self.n_chars = len(self.chars)
@@ -80,7 +80,7 @@ class Vocab(object):
         self.word_dict = {w: i for i, w in enumerate(self.words)}
         self.char_dict = {c: i for i, c in enumerate(self.chars)}
         self.puncts = sorted(i for word, i in self.word_dict.items()
-                             if regex.match(r'\p{P}+$', word))
+                             if self.is_punctuation(word))
         self.n_words = len(self.words)
         self.n_chars = len(self.chars)
 
@@ -103,3 +103,7 @@ class Vocab(object):
         vocab = cls(words, chars, rels)
 
         return vocab
+
+    @classmethod
+    def is_punctuation(cls, word):
+        return all(unicodedata.category(char).startswith('P') for char in word)
