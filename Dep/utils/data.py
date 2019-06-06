4d39e1c08800ae95267b2d9a09524a8c4e60bc1f
HMJW <767257113@qq.com>
2019-06-06 16:25:00 +0800

    modify bucket

--- Dep/utils/data.py
+++ Dep/utils/data.py
@@ -13,10 +13,10 @@ def kmeans(x, k):
     dists, y = torch.abs_(x.unsqueeze(-1) - c).min(dim=-1)
 
     while old is None or not c.equal(old):
-        # handle the empty clusters
+        # if an empty cluster is encountered,
+        # choose the farthest datapoint from the biggest cluster
+        # and move that the empty one
         for i in range(k):
-            # choose the farthest datapoint from the biggest cluster
-            # and move that the empty cluster
             if not y.eq(i).any():
                 mask = y.eq(torch.arange(k).unsqueeze(-1))
                 lens = mask.sum(dim=-1)
@@ -44,20 +44,26 @@ def collate_fn(data):
 
 class TextSampler(Sampler):
 
-    def __init__(self, lengths, batch_size, n_buckets, shuffle=False):
-        self.lengths = lengths
+    def __init__(self, buckets, batch_size, shuffle=False, max_len=800):
         self.batch_size = batch_size
         self.shuffle = shuffle
-        # NOTE: the final bucket count is less than or equal to n_buckets
-        self.sizes, self.buckets = kmeans(x=lengths, k=n_buckets)
-        self.chunks = [max(size * len(bucket) // self.batch_size, 1)
-                       for size, bucket in zip(self.sizes, self.buckets)]
+        self.sizes, self.buckets = zip(*[
+            (size, bucket) for size, bucket in buckets.items()
+        ])
+        # number of chunks in each bucket
+        self.chunks = [
+            max(round(size * len(bucket) / min(max_len * size, batch_size)), 1)
+            for size, bucket in zip(self.sizes, self.buckets)
+        ]
 
     def __iter__(self):
         # if shuffle, shffule both the buckets and samples in each bucket
         range_fn = torch.randperm if self.shuffle else torch.arange
-        for i in range_fn(len(self.buckets)):
-            for batch in range_fn(len(self.buckets[i])).chunk(self.chunks[i]):
+        for i in range_fn(len(self.buckets)).tolist():
+            split_sizes = [(len(self.buckets[i]) - j - 1) // self.chunks[i] + 1
+                           for j in range(self.chunks[i])]
+            # DON'T use `torch.chunk` which may return wrong number of chunks
+            for batch in range_fn(len(self.buckets[i])).split(split_sizes):
                 yield [self.buckets[i][j] for j in batch.tolist()]
 
     def __len__(self):
@@ -70,6 +76,9 @@ class TextDataset(Dataset):
         super(TextDataset, self).__init__()
 
         self.items = items
+        # NOTE: the final bucket count is less than or equal to n_buckets
+        self.centroids, self.clusters = kmeans(x=[len(i) for i in items[0]],
+                                               k=n_buckets)
 
     def __getitem__(self, index):
         return tuple(item[index] for item in self.items)
@@ -78,14 +87,13 @@ class TextDataset(Dataset):
         return len(self.items[0])
 
     @property
-    def lengths(self):
-        return [len(i) for i in self.items[0]]
+    def buckets(self):
+        return dict(zip(self.centroids, self.clusters))
 
 
-def batchify(dataset, batch_size, n_buckets=1, shuffle=False):
-    batch_sampler = TextSampler(lengths=dataset.lengths,
+def batchify(dataset, batch_size, shuffle=False):
+    batch_sampler = TextSampler(buckets=dataset.buckets,
                                 batch_size=batch_size,
-                                n_buckets=n_buckets,
                                 shuffle=shuffle)
     loader = DataLoader(dataset=dataset,
                         batch_sampler=batch_sampler,
